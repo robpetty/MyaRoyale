5c8a1e3eac772d0af62e7a62d8b1e10cd7afd0aa
Robert Petty <liquidgod@hotmail.com>
2020-06-26 23:15:54 -0700

    Fix missing PlayrerInfo issue.
    Seems earlier revert deleted PlayerInfo.  Readded from Github by copying into a new script file locally.  Readded the four charactesrs to the PlayerInfo gameobject.

---
+++ Assets/Scripts/Photon/PlayerInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public static PlayerInfo pi;
+    public int mySelectedCharacter;
+    public GameObject[] allCharacters;
+
+    private void OnEnable()
+    {
+        if (PlayerInfo.pi == null)
+        {
+            PlayerInfo.pi = this;
+        }
+        else
+        {
+            if (PlayerInfo.pi != this)
+            {
+                Destroy(PlayerInfo.pi.gameObject);
+                PlayerInfo.pi = this;
+            }
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("MyCharacter") == true)
+        {
+            mySelectedCharacter = PlayerPrefs.GetInt("MyCharacter");
+        }
+        else
+        {
+            mySelectedCharacter = 0;
+            PlayerPrefs.SetInt("MyCharacter", mySelectedCharacter);
+        }
+    }
+}
